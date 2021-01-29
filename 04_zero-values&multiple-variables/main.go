4091b8961d20a2c7cf5699e16d2fba7b300159e2
Aykut Ulış <aykutulis@hotmail.com>
2021-01-29 03:54:44 +0300

    Zero values & Multiple variables

---
+++ 04_zero-values&multiple-variables/main.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	/* 	var (
+		name      string = "Arthur"
+		age       int    = 50
+		isMarried bool   = false
+		weight float32 = 75.5
+		height int     = 171
+	) */
+
+	/* var name, age, isMarried, weight, height = "Arthur", 50, false, 72.5, 171 */
+
+	/* name, age, isMarried, weight, height := "Arthur", 50, false, 72.5, 171 */
+
+	var isMarried bool
+
+	fmt.Println(isMarried) // string --> "", numeric --> 0, bool --> false
+	/* 	fmt.Println(age)
+	   	fmt.Println(isMarried)
+	   	fmt.Println(weight)
+	   	fmt.Println(height) */
+
+}
