3d31a82b6d5a6bab2e056bcd2e51c48e1fff43b4
Aykut Ulış <aykutulis@hotmail.com>
2021-01-29 03:41:39 +0300

    Basic data types

---
+++ 03_basic-data-types/main.go
@@ -0,0 +1,27 @@
+// Package Clause
+package main
+
+// Import Statement
+import "fmt"
+
+// My Codes
+func main() {
+
+	var name = "Arthur"
+
+	var age int8 = -128
+
+	var isMarried bool = false
+
+	var weight float32 = 75.5
+
+	fmt.Println(name)
+	fmt.Println(age)
+	fmt.Println(isMarried)
+	fmt.Println(weight)
+
+	fmt.Printf("%T\n", name)
+	fmt.Printf("%T\n", age)
+	fmt.Printf("%T\n", isMarried)
+	fmt.Printf("%T\n", weight)
+}
