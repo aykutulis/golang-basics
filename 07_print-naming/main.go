b40278112df7aeb12944e9303f34898e7c9e5aa5
Aykut Ulış <aykutulis@hotmail.com>
2021-01-29 06:05:20 +0300

    Print & Naming

---
+++ 07_print-naming/main.go
@@ -0,0 +1,48 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	/* 	fmt.Println("Hello!")
+	   	fmt.Print("Hello!")
+	   	fmt.Println("")
+	   	fmt.Printf("Hello!") */
+
+	/* 	name := "Alan" */
+
+	/* 	fmt.Print(name)
+	   	fmt.Println(name)
+	   	fmt.Printf(name) */
+
+	/* 	fmt.Print("My name is", name)
+	   	fmt.Println("")
+	   	fmt.Println("My name is", name)
+	   	fmt.Printf("My name is %X %T", name, name) */
+
+	x := 100
+	y := 50
+	z := 70
+	fmt.Printf("%b %d %o\n", x, y, z)
+
+	name, age := "Alan", 35
+
+	fmt.Print("My name is ", name, ", and I am ", age, "\n")
+	fmt.Println("My name is", name, "and I am", age)
+	fmt.Printf("My name is %v, and I am %v", name, age)
+
+	// Visibility
+
+	/* 	fmt.Println(x)
+	   	myFunc() */
+
+	/* 	// Go camel case
+	   	var coinType string
+	   	var custName string
+
+	   	var URL // Upper case
+	   	var HTTP // Upper case
+		i , j , k // index
+	*/
+
+}
