689f0a2f48f1bded6334eeba322997bc3a33decc
Aykut Ulış <aykutulis@hotmail.com>
2021-01-29 03:19:20 +0300

    Variable declaration

---
+++ 02_variable_declaration/main.go
@@ -0,0 +1,20 @@
+// Package Clause
+package main
+
+// Import Statement
+import "fmt"
+
+// My Codes
+func main() {
+
+	var firstName, surName string = "Alan", "Turing"
+
+	var age int
+	age = 25
+
+	isMarried := false
+
+	fmt.Println(firstName, surName)
+	fmt.Println(age)
+	fmt.Println(isMarried)
+}
